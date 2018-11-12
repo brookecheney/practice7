730a10917e35a30364155acc7f17fc8e2402a89f
Brooklyne Cheney <brookecheney@hotmail.com>
2018-11-12 13:48:14 -0700

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div >
+      <h1>This is an about page</h1>
+      <ul id="example-1">
+          <li v-for="film in allFilms" :key="film.episode_id">
+              {{ film.title }}
+
+          </li>
+</ul>
+</div>
+</template>
+
+<script>
+
+import { films } from '../assets/films'
+import { starships } from '../assets/starships'
+
+export default {
+  data: function() {
+      return {
+          allFilms: films
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ layout.vue
@@ -0,0 +1,33 @@
+<template>
+
+
+<v-app>
+<v-navigation-drawer app>Nav </v-navigation-drawer>
+
+<v-toolbar app>Toolbar</v-toolbar>
+<v-content>
+    <v-container fluid>
+
+
+
+        <router-view></router-view>
+    </v-container>
+    <v-footer app>
+
+    </v-content>
+    <v-footer app>Footer</v-footer>
+    </v-app>
+    </template>
+    <script>
+    export default {
+
+    }
+
+</script>
+
+
+<style scoped>
+
+
+
+</style>
